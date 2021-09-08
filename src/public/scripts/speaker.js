e6ef6dbdfa2848dfddf002f76354273438556c11
neverm1ndo <it@nmnd.ru>
2021-09-09 00:02:16 +0300

    add compressor to speaker

--- src/public/scripts/speaker.js
+++ src/public/scripts/speaker.js
@@ -1,160 +1,110 @@
 let ws;
-class Filter {
-  constructor() {
-  }
-  static separate (text) {
-    const schedule = ['/^', 'пидор', 'пидр', 'пидарас', 'пидорас'];
-    schedule.forEach((word) => {
-    text = text.replace(new RegExp(`${word}`, 'gi'), 'уууууу');
-    });
-    return text;
-  }
-}
 class Player {
   constructor() {
-    this.audio = new Audio();
-    this.audio.src = undefined;
-    this.audio.controls = false;
-    this.audio.autoplay = true;
-    this.audio.volume = window.localStorage.getItem('volumePlayer');
-
-    document.body.appendChild(this.audio);
-
-    this.playerRanger = document.querySelector('#volume2');
-    this.playerRanger.value = this.audio.volume*100;
-    document.querySelector('#sou_vol').innerHTML = this.playerRanger.value;
-    this.playerRanger.addEventListener('input', () => {
-      this.audio.volume = this.playerRanger.value/100;
-      document.querySelector('#sou_vol').innerHTML = this.playerRanger.value;
-      window.localStorage.setItem('volumePlayer', this.audio.volume);
+    this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+    this.source = this.ctx.createBufferSource();
+    this.gainNode = this.ctx.createGain();
+    this.source.addEventListener('ended', () => {
+      this.source.stop();
+      this.source = null;
+      this.source = this.ctx.createBufferSource();
     });
+    this.gainNode.gain.value = window.localStorage.getItem('gain');
+
+    this.compressor = this.ctx.createDynamicsCompressor();
+    this.compressor.threshold.value = window.localStorage.getItem('threshold');
+    this.compressor.knee.value = window.localStorage.getItem('knee');
+    this.compressor.ratio.value = window.localStorage.getItem('ratio');
+    this.compressor.attack.value = window.localStorage.getItem('attack');
+    this.compressor.release.value = window.localStorage.getItem('release');
+
+    this.source.connect(this.gainNode);
+    if (window.localStorage.getItem('compressor') === 'true') {
+      this.compressor.connect(this.gainNode);
+    }
+    this.gainNode.connect(this.ctx.destination);
+
+    this.playerRanger = document.querySelector('#gain');
+    this.playerRanger.value = this.gainNode.gain.value*100;
+    document.querySelector('#gain_value').innerHTML = this.playerRanger.value;
+    this.playerRanger.addEventListener('input', () => {
+      this.gainNode.gain.value = this.playerRanger.value/100;
+      document.querySelector('#gain_value').innerHTML = this.playerRanger.value;
+      window.localStorage.setItem('gain', this.gainNode.gain.value);
+    });
+    this.addListeners();
   }
   set vol(volume) {
     this.audio.volume = volume;
   }
-  loadSource(path) {
-    this.audio.src = path;
-    this.audio.load();
-    this.audio.play();
-  };
+  addListeners() {
+    const compressorSwitch = document.querySelector('#compressor');
+    compressorSwitch.checked = window.localStorage.getItem('compressor') === 'true';
+    const knee = document.querySelector('#knee');
+    const ratio = document.querySelector('#ratio');
+    const attack = document.querySelector('#attack');
+    const release = document.querySelector('#release');
+    const threshold = document.querySelector('#threshold');
+    [knee, ratio, attack, release, threshold].forEach((input) => {
+      input.value = window.localStorage.getItem(input.name);
+      input.disabled = !compressorSwitch.checked;
+      input.addEventListener('input', (event) => {
+          this.compressor[input.name].value = input.value;
+        window.localStorage.setItem(input.name, input.value);
+      });
+    })
+    compressorSwitch.addEventListener('change', (event) => {
+      window.localStorage.setItem('compressor', compressorSwitch.checked);
+      player.switchCompressor(compressorSwitch.checked);
+      this.toggleInputs(compressorSwitch.checked, [knee, ratio, attack, release, threshold]);
+    });
+  }
+  toggleInputs(value, inputs) {
+    inputs.forEach((input) => {
+      input.disabled = !value;
+    });
+  }
+  play (path) {
+    new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest();
+      request.open('GET', path, true);
+      request.responseType = 'arraybuffer';
+      request.onload = () => {
+        console.log(request.response)
+        this.ctx.decodeAudioData(request.response,
+          (buffer) => {
+            this.source.buffer = buffer;
+            this.source.loop = false;
+            resolve();
+          },
+          (e) => {
+            console.log("Error with decoding audio data" + e.err);
+            reject();
+          });
+        }
+        request.send();
+    }).then(() => {
+      this.source.connect(this.compressor);
+      this.source.start(0);
+    })
+	}
+  switchCompressor(value) {
+    if (!value) {
+			this.compressor.disconnect(0);
+		} else {
+      this.compressor.connect(this.gainNode);
+		}
+  }
 }
 
 let player;
-player = new Player();
+const accept = document.querySelector('#accept');
+accept.addEventListener('click', (event) => {
+  player = new Player();
+  event.target.style.display = 'none';
+});
 
-class Speaker2 {
-  constructor() {
-    this.synth = window.speechSynthesis;
-    // this.synth2 = window.speechSynthesis;
-    this.controls = {
-      accept: document.querySelector('#accept'),
-      volume: document.querySelector('#volume')
-    }
-    this.volume = null;
-    if (window.localStorage.getItem('volume')) {
-      this.volume = window.localStorage.getItem('volume');
-    } else {
-      this.volume = 1;
-    }
-    this.controls.volume.value = this.volume*100;
-
-    this.indicator = document.querySelector('#sp_vol');
-    this.indicator.innerHTML = this.controls.volume.value;
-
-    this.controls.accept.addEventListener('click', (e)=> {
-      e.target.style.display = 'none';
-      this.controls.volume.style.display = 'block';
-    });
-
-    this.controls.volume.addEventListener('input', (e) => {
-      this.volume = e.target.value/100;
-      this.indicator.innerHTML = e.target.value;
-      window.localStorage.setItem('volume', this.volume);
-    });
-    this.pitch = 1;
-    this.rate = 1;
-
-    this.voices = [];
-  }
-
-  async populateVoiceList() {
-    await new Promise((resolve, reject) => {
-      let id;
-      id = setInterval(() => {
-          if (this.synth.getVoices().length !== 0) {
-              resolve(this.synth.getVoices());
-              clearInterval(id);
-          } else {
-            reject('Reject in promise: setSpeech()');
-          }
-      }, 100);
-    }).then((voices) => { this.voices = voices });
-    this.voices = this.voices.sort(function (a, b) {
-      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
-      if ( aname < bname ) return -1;
-      else if ( aname == bname ) return 0;
-      else return +1;
-    });
-  };
-
-  speak(text, pitch) {
-    if (pitch == undefined) { pitch = this.pitch};
-    if (text !== '') {
-      let selectedOption;
-      if (text.includes(' --p ')) {
-        let params = text.split(' --p ');
-            text = params[0];
-            selectedOption = this.voices[params[1].split('|')[0]].name;
-            pitch = +params[1].split('|')[1];
-      } else {
-          selectedOption = 'Google русский';
-      }
-      let utterThis = new SpeechSynthesisUtterance(text);
-      for(let i = 0; i < this.voices.length ; i++) {
-        if(this.voices[i].name === selectedOption) {
-          utterThis.voice = this.voices[i];
-          break;
-        }
-      }
-      utterThis.pitch = pitch;
-      utterThis.rate = this.rate;
-      utterThis.volume = this.volume;
-      this.synth.speak(utterThis);
-    }
-  }
-
-  parseSong(text) {
-    const rText = new RegExp('\\w+\=\\w','g');
-    const matched = text.match(rText);
-    let parsed = [];
-    matched.forEach((m, index) => {
-      const t = matched[index].split('=');
-      parsed.push({text: t[0], pitch: Sheet.note[t[1]]});
-    });
-    console.log(parsed);
-    return parsed;
-  }
-
-  async sing (text) {
-    const parsed = this.parseSong(text);
-    for (let i = 0; parsed.length > i; i+=1) {
-      await new Promise((resolve, reject) => {
-        this.speak(parsed[i].text, parsed[i].pitch);
-        let int = setInterval(()=> {
-          if (this.synth.pending) {
-            clearInterval(int);
-          }
-          if (!this.synth.speaking) {
-            resolve(+parsed[i].pitch);
-          }
-        }, 10);
-      });
-    }
-  }
-}
-
-const speaker = new Speaker2();
-      speaker.populateVoiceList();
+const indicator = document.querySelector('#indicator');
 
 (function setConnection() {
   console.log(window.location.host);
@@ -162,6 +112,8 @@ const speaker = new Speaker2();
   ws.onopen = function() {
     console.log("Соединение установлено.");
     ws.send(JSON.stringify({event: 'speaker-connection'}));
+    indicator.classList.remove('bg-red');
+    indicator.classList.add('bg-green');
   };
 
   ws.onclose = function(event) {
@@ -169,6 +121,8 @@ const speaker = new Speaker2();
       console.log('Соединение закрыто чисто');
     } else {
       console.log('Обрыв соединения');
+      indicator.classList.remove('bg-green');
+      indicator.classList.add('bg-red');
       setTimeout(()=> {
         setConnection();
       }, 5000)
@@ -180,12 +134,9 @@ const speaker = new Speaker2();
     let depeche = JSON.parse(event.data);
     console.log(depeche);
     switch (depeche.event) {
-      case 'hl_msg':
-      console.log(depeche.msg);
-        speaker.speak(Filter.separate(depeche.msg));
-      break;
       case 'play-sound':
-        player.loadSource(depeche.msg);
+        if (!player) break;
+        player.play(depeche.msg);
       break;
       case 'connection':
         console.log(depeche.msg);
