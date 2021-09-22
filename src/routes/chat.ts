d3b242538f62251502e38a99226dbefea170fc6f
neverm1ndo <it@nmnd.ru>
2021-09-22 21:59:11 +0300

    add sort to message history hf

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -21,7 +21,7 @@ router.post('/add-lurker', corsOpt, (req: Request, res: Response,) => {
 });
 router.get('/last', corsOpt, (req: Request, res: Response,) => {
   if (!req.query.channel) res.sendStatus(BAD_REQUEST);
-  MESSAGE.find({ channel: '#' + req.query.channel }, [], { sort: '-date', limit: 30 }, (err: any, messages: Document[]) => {
+  MESSAGE.find({ channel: '#' + req.query.channel }, [], { sort: {date: -1}, limit: 30 }, (err: any, messages: Document[]) => {
     if (err) return logger.err(err);
     res.send(messages);
   });
