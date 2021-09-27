cf6e4bcc22994c421cf89954b39df7d6d7c592ae
neverm1ndo <it@nmnd.ru>
2021-09-27 03:26:16 +0300

    add nickname colors control

--- src/public/scripts/chat.app.js
+++ src/public/scripts/chat.app.js
@@ -9,6 +9,81 @@ const defaultBadges = {
   'verified': '/img/vf.png',
   'prime': '/img/prime.png',
 };
+class HEX {
+  constructor(color) {
+    if (typeof color !== 'string') throw console.error(Error(color + ' is not a string!'));
+    if (color.startsWith('#')) color = color.substr(1);
+    this.color = parseInt(color, 16);
+    this.r = parseInt(color.substr(0, 2), 16);
+    this.g = parseInt(color.substr(2, 2), 16);
+    this.b = parseInt(color.substr(4, 2), 16);
+  }
+  #setSpecters() {
+    this.r = parseInt(this.toString().substr(0, 2), 16);
+    this.g = parseInt(this.toString().substr(2, 2), 16);
+    this.b = parseInt(this.toString().substr(4, 2), 16);
+  }
+  #setColor() {
+    this.color = parseInt(this.toString().substr(1), 16);
+  }
+  blend(hex) {
+    if (!(hex instanceof HEX)) {
+      throw console.error(Error(hex + ' is not a HEX instance!'));
+    }
+    this.color = this.color + hex.color;
+    this.#setSpecters();
+    return this;
+  }
+  toString() {
+    const c = {
+      r: this.r.toString(16),
+      g: this.g.toString(16),
+      b: this.b.toString(16)
+    };
+    const result = '#' + ('0').repeat(2 - c.r.length) + c.r + ('0').repeat(2 - c.g.length) + c.g + ('0').repeat(2 - c.b.length) + c.b;
+    return result;
+  }
+  valueOf() {
+    return this.color;
+  }
+  saturation(amount) {
+    amount = amount/100;
+    if (amount < 0) {
+      console.warn('Saturation percentage must be more than 0', amount);
+      return this;
+    }
+    const gray = this.r * 0.3086 + this.g * 0.6094 + this.b * 0.0820;
+    this.r = Math.round(this.r * amount + gray * (1-amount));
+    this.g = Math.round(this.g * amount + gray * (1-amount));
+    this.b = Math.round(this.b * amount + gray * (1-amount));
+    this.#setColor();
+    return this;
+  }
+  contrast(amount) {
+    amount = amount/100;
+    if (amount < 0) {
+      return this;
+    }
+    const col = Math.max.apply(null, [this.r, this.g, this.b]);
+    this.r = Math.min(255, Math.round((this.r/col)*this.r + (amount*col)));
+    this.g = Math.min(255, Math.round((this.g/col)*this.g + (amount*col)));
+    this.b = Math.min(255, Math.round((this.b/col)*this.b + (amount*col)));
+    this.#setColor();
+    return this;
+  }
+  brightness(amount) {
+    amount = amount/100;
+    if (amount < 0) {
+      console.warn('Brightness percentage must be more than 0', amount);
+      return this;
+    }
+    this.r = Math.min(255,Math.floor(this.r + 255 - (amount*255)));
+    this.g = Math.min(255,Math.floor(this.g + 255 - (amount*255)));
+    this.b = Math.min(255,Math.floor(this.b + 255 - (amount*255)));
+    this.#setColor();
+    return this;
+  }
+}
 class Cookies {
   static getCookie(name) {
     let matches = document.cookie.match(new RegExp(
@@ -138,18 +213,24 @@ class MessageControlButton extends HTMLButtonElement {
 class ChatMessage extends HTMLDivElement {
   constructor(tags, message, self, date) {
     super();
+    let splitter = ': ';
+    const color = new HEX(tags.color);
+
     this.body = document.createElement('div');
     this.nickname = document.createElement('span');
     this.tags = tags;
     this.links = [];
     this.nickname.classList.add('nickname');
     this.nickname.innerHTML = tags['display-name'];
-    this.nickname.style.color = tags.color;
+    this.nickname.style.color = color < 0x505050?color.brightness(70).contrast(30).toString():color.toString();
+    if (tags['message-type'] === "action") {
+      this.body.style.color = this.nickname.style.color;
+      splitter = '';
+    };
     this.classList.add('card');
     this.body.classList.add('card-body');
-    if (tags['message-type'] === "action") this.body.style.color = this.nickname.style.color;
     this.body.dataset.date = (this.timestamp(date));
-    this.body.innerHTML = ': ' + this.pretty(tags, message);
+    this.body.innerHTML = splitter + this.pretty(tags, message);
     this.addTooltipsToEmotes();
     this.body.prepend(this.nickname);
     if (tags.badges) {
