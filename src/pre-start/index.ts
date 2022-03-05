18f26586665b2a7a67fbcd65ab86a09ac0b2c833
neverm1ndo <it@nmnd.ru>
2022-03-05 18:26:24 +0300

    fix start option dsub alias

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -4,7 +4,7 @@ import commandLineArgs from 'command-line-args';
 // Setup command line options
 const options = commandLineArgs([
   { name: 'works', defaultValue: false, alias: 'w', type: Boolean},
-  { name: 'dsub', defaultValue: false, alias: 'ds', type: Boolean}
+  { name: 'dsub', defaultValue: false, alias: 'd', type: Boolean}
 ]);
 
 export default options;
