f80c5d48b5fdd3b837b6fc9d807e10d754397b12
neverm1ndo <it@nmnd.ru>
2022-03-11 23:31:16 +0300

    remove set Content-Security-Policy from dashboard render

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -4,8 +4,7 @@ import { checkSession } from '@shared/functions';
 const router = Router();
 
 router.get('/', checkSession, (req: Request, res: Response) => {
-  res.set('Content-Security-Policy', 'default-src *')
-     .render('dashboard', { session: req.session });
+  res.render('dashboard', { session: req.session });
 });
 
 export default router;
