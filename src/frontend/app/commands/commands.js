35adfaec032c6c8020fc9b1a24d0562236d63cba
neverm1ndo <it@nmnd.ru>
2022-03-28 08:07:01 +0300

    fixes command landing page

--- src/frontend/app/commands/commands.js
+++ src/frontend/app/commands/commands.js
@@ -4,5 +4,7 @@ import Collapse from 'bootstrap/js/dist/collapse';
 
 const collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
 const collapseList = collapseElementList.map(function (collapseEl) {
-  return new Collapse(collapseEl)
-})
+  return new Collapse(collapseEl, {
+    toggle: false
+  });
+});
--- src/routes/landings.ts
+++ src/routes/landings.ts
@@ -7,7 +7,14 @@ const router = Router();
 router.get('/:username', (req: Request, res: Response) => {
   USER.findOne({'user.login': req.params.username }, (err: any, user: any) => {
     if (err) return logger.err(err, true);
-    res.render('commands', { sounds: user.settings.sounds, custom: user.settings.commands, username: req.params.username });
+    res.render('commands', {
+      sounds: user.settings.sounds,
+      custom: user.settings.commands,
+      user: {
+        username: user.user.display_name,
+        avatar: user.user.profile_image_url
+      }
+    });
   })
 });
 
